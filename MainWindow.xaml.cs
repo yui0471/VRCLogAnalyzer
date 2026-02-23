01f6064c6fa57be847eb50f2afd1fd9135703bd1
風庭ゆい <yui.kazeniwa@gmail.com>
2026-02-24 00:58:36 +0900

    using System.Globalization;を追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 using System.IO;
 
